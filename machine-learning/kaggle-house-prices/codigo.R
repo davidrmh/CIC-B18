7948da18b530da09cda02762a8ca41167c6fb4bc
David Montalvan <davidricardo888@hotmail.com>
2018-08-18 18:26:02 -0500

    Add functions creaCSVKaggle, quitaExtremos, graficoDispersion, cargaDatos

--- machine-learning/kaggle-house-prices/codigo.R
+++ machine-learning/kaggle-house-prices/codigo.R
@@ -1,8 +1,14 @@
-#modelo<-lm(SalePrice ~ YearBuilt + OverallQual + LotArea + GarageCars ,data=entrenamiento)
-#modelo <- lm(SalePrice ~ I(YearBuilt^3) + OverallQual:LotArea + LotArea + GarageCars:LotArea,data=entrena2)
+#modelo <- lm(SalePrice ~ I(YearBuilt^3) + OverallQual:LotArea + LotArea + GarageCars:LotArea,data=entrena)
+#modelo <- lm(SalePrice ~ LotArea, data = entrena)
 #predicciones<-predict(modelo,newdata=prueba)
 
 
+library(ggplot2)
+
+##========================================##
+## Función para calcular el RMSE del
+## logaritmo de las predicciones
+##========================================##
 rmse<-function(predicciones,benchmark){
 
   #Se calcula el promedio sin incluir los NA
@@ -31,6 +37,70 @@ rmse<-function(predicciones,benchmark){
   error<-sqrt(mean(diferencias^2))
 
   return (error)
-
-
 }
+
+##========================================##
+## Función para cargar los datos del csv
+##========================================##
+cargaDatos <- function(path = "train.csv"){
+  data <- read.csv(path)
+  return(data)
+}
+
+##========================================##
+## Función para obtener gráficos de
+## dispersión
+##
+## Argumentos:
+## x: string con el nombre de la variable x
+##
+## y: string con el nombre de la variable y
+##
+## z: string con el nombre de la variable z
+## la variable z es para obtener el color
+## de los puntos
+## 
+## datos: data.frame de donde se obtienen
+## los datos
+##========================================##
+graficoDispersion <- function(x,y,z,datos){
+  g <- ggplot(data=datos, mapping = aes_string(x = x, y = y, colour = z)) + geom_point()
+  g
+}
+
+##========================================##
+## Función para remover las observaciones
+## con SalePrice mayores / menores a 
+## dos desviaciones estándar
+##
+## Atributos:
+## datos: data.frame con los datos
+##========================================##
+quitaExtremos <- function(datos){
+  nOrig <- dim(datos)[1] # Num Observaciones originales
+  media <- mean(datos$SalePrice)
+  desv <- sd(datos$SalePrice)
+  limiteSup <- media + 2*desv
+  limiteInf <- media - 2*desv
+  indicesRemover <- which(datos$SalePrice > limiteSup | datos$SalePrice < limiteInf )
+  datos = datos[-indicesRemover,]
+  nNew <- dim(datos)[1] #Num de observaciones entre los límites
+  print(paste("Se quitaron ",round(100*(nOrig - nNew)/nOrig,2), "% de observaciones",sep=""))
+  return(datos)
+}
+
+##========================================##
+## Función para crear el archivo csv
+## que se envía a Kaggle
+##
+## Atributos:
+## predicciones: Vector con las predicciones del modelo
+## objetivo: Data.frame con el contenido del archivo "sample_submission.csv"
+##========================================##
+creaCSVKaggle <- function(predicciones,objetivo){
+  #Número de predicciones
+  numPred <- length(predicciones)
+  datos <- data.frame(Id = objetivo$Id, SalePrice = predicciones)
+  write.csv(datos, file = "David-Montalvan-House-Prices-Kaggle.csv", row.names = FALSE)
+}
+
