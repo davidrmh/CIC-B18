3cd09dc4d6c8cfc24eba7ac04a802f6635241618
David <davidricardo888@hotmail.com>
2018-10-30 18:49:40 -0600

    Add pi_montecarlo.c

---
+++ computo-paralelo/pi_montecarlo.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+/* Programa para estimar PI utilizando simulación Monte Carlo
+
+Para compilar:
+gcc -I /usr/local/include/gsl/ -o pi_montecarlo pi_montecarlo.c -lgsl -lgslcbla
+
+Uso
+./pi_montecarlo NUM_SIM
+*/
+
+int main(int argc, char *argv[]){
+
+  // argv[1] es el número de simulaciones
+  long num_sim = atoi(argv[1]);
+
+  //radio
+  float radio = 1.0;
+  float radio2 = radio*radio;
+
+  //Para la generación de números aleatorios
+  const gsl_rng_type * T;
+  gsl_rng * r;
+  gsl_rng_env_setup();
+  T = gsl_rng_default;
+  r = gsl_rng_alloc (T);
+  double x, y, u;
+
+  //Para el loop
+  int i;
+
+  //Contador de puntos que caen en el círculo
+  long contador_adentro = 0;
+
+  for(i = 0; i < num_sim; i++){
+
+    u = gsl_rng_uniform (r);
+    x = -radio + (radio + radio)*u;
+    u = gsl_rng_uniform (r);
+    y = -radio + (radio + radio)*u;
+
+    if((x*x + y*y) <= radio2){
+      contador_adentro = contador_adentro + 1;
+    }
+  }
+
+  //calcula PI
+  float pi = (float) 4*contador_adentro / num_sim;
+
+  printf("PI estimado %.5f\n", pi);
+
+  return 0;
+}
