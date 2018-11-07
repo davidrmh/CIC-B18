a7463f0533fe0b98a4e7ff5c4fdec6dab2eee947
David <davidricardo888@hotmail.com>
2018-11-06 20:32:09 -0600

    Add diccionario_imagenes function

--- machine-learning/kaggle-galaxy-zoo/codigos/preprocesamiento.py
+++ machine-learning/kaggle-galaxy-zoo/codigos/preprocesamiento.py
@@ -2,6 +2,8 @@
 
 from PIL import Image
 import numpy as np
+import pandas as pd
+import pickle
 
 ##==============================================================================
 ## Función para convertir una imagen en un arreglo numpy
@@ -70,3 +72,59 @@ def transpon_imagen(imagen, tipo):
 ## Función para recortar una imagen (PENDIENTE)
 ##==============================================================================
 #def recorta_imagen(imagen, caja = (101, 101, 101, 101)):
+
+
+##==============================================================================
+## Función para crear un diccionario que contendrá los siguientes elementos
+## GalaxyID (Key)
+## .. target: Numpy array con las probabilidades del conjunto de entrenamiento
+## .. imagen: Objeto creado con el módulo PIL (Image.open)
+##==============================================================================
+def diccionario_imagenes(real = False):
+    '''
+    ENTRADA
+    real: Boolean, True => Obtener todas las imágenes del conjunto de entrenamiento
+    False => Obtener sólamente las primeros 20 imágenes del conjunto de
+    entrenamiento
+
+    SALIDA
+    diccionario con la información del target y las imágenes
+    '''
+    #lee el csv con los IDs y los targets
+    arch_csv = pd.read_csv('../all/training_solutions_rev1.csv')
+
+    if real:
+        n = arch_csv.shape[0]
+    else:
+        n = 20
+
+    #ruta en donde se guardan las imágenes
+    ruta = '../all/images_training_rev1/'
+
+    #ruta en donde se guardará la información del diccionario
+
+    #Llena el diccionario
+    dicc = {}
+    for i in range(0, n):
+
+        #obtiene la ruta de la imagen
+        galaxy_id = str(arch_csv.iloc[i,0])
+        ruta_imagen = ruta + galaxy_id + '.jpg'
+
+        #abre la imagen
+        imagen = Image.open(ruta_imagen)
+
+        #obtiene las probabilidades objetivo
+        targets = np.array(arch_csv.iloc[i,1:])
+
+        #Llena el diccionario
+        dicc[galaxy_id] = {}
+        dicc[galaxy_id]['target'] = targets
+        dicc[galaxy_id]['imagen'] = imagen
+
+    #guarda el diccionario
+    arch_salida = open('diccionario_imagenes', 'wb')
+    pickle.dump(dicc, arch_salida)
+    arch_salida.close()
+
+    return dicc
