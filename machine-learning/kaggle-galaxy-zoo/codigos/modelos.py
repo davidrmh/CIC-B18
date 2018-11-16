716b1b7bd5118455f17a6abb58a3208aee0d05e0
David <davidricardo888@hotmail.com>
2018-11-15 19:32:27 -0600

    Agrega función carga_imagenes_arreglo

--- machine-learning/kaggle-galaxy-zoo/codigos/modelos.py
+++ machine-learning/kaggle-galaxy-zoo/codigos/modelos.py
@@ -115,3 +115,81 @@ def entrena_modelo(model, ruta_entrenamiento, csv_target, epochs=20, loss='mean_
     print 'Inicio ' + inicio + ' Fin ' + fin
 
     return model, historia
+
+
+##==============================================================================
+## Funcion para evaluar un conjunto de imagenes de acuerdo a un modelo dado
+## CARGA EN MEMORIA
+##==============================================================================
+def carga_imagenes_arreglo(ruta, model, num_imagenes = 100, ext = '.jpg'):
+    '''
+    ENTRADA
+    ruta: String con la ruta de la carpeta que contiene las imagenes
+
+    model: Modelo entrenado
+
+    num_imagenes: Entero que indica la cantidad de imagenes que se tomaran
+    de la carpeta. si num_imagenes = '' entonces obtiene todos los archivos
+
+    ext: String con la extension de las imagenes
+
+    SALIDA
+    Arreglo de numpy con las imagenes
+    '''
+
+    #Obtiene la ruta de cada archivo en la carpeta
+    lista_arch = pre.lista_archivos(ruta, ext)
+
+    #Selecciona los archivos a utilizar
+    if num_imagenes != '':
+        lista_arch = pre.np.random.choice(lista_arch, size = num_imagenes, replace = False)
+
+    #para almacenar cada imagen y cada id
+    x_test = []
+    ids = []
+
+    for arch in lista_arch:
+        #Abre la imagen
+        imagen = pre.Image.open(arch)
+
+        #determina si es una imagen a color
+        if 'BW' in arch:
+            col = False
+        else:
+            col = True
+
+        #Convierte la imagen en un arreglo numerico
+        arreglo = pre.imagen_a_arreglo(imagen, col)
+
+        #redimensiona el arreglo para considerar el numero de canales
+        if col:
+            arreglo.shape = (3, arreglo.shape[0], arreglo.shape[1])
+        else:
+            arreglo.shape = (1, arreglo.shape[0], arreglo.shape[1])
+
+        #agrega el arreglo a x_test
+        x_test.append(arreglo)
+
+        #cierra la imagen
+        imagen.close()
+
+        #obtiene el Id de la imagen
+        id_imagen = arch.split('/')[-1].split('.')[0]
+        ids.append(id_imagen)
+
+    #convierte x_test en un numpy array
+    x_test = pre.np.array(x_test)
+    ids = pre.np.array(ids)
+
+    #columnas
+    columnas = ['Class1.1', 'Class1.2', 'Class1.3', 'Class2.2', 'Class2.2', 'Class3.1',
+    'Class3.2', 'Class4.1', 'Class4.2', 'Class5.1', 'Class5.2', 'Class5.3', 'Class5.4',
+    'Class6.1', 'Class6.2', 'Class7.1', 'Class7.2', 'Class7.3', 'Class8.1', 'Class8.2',
+    'Class8.3', 'Class8.4', 'Class8.5', 'Class8.6', 'Class8.7', 'Class9.1', 'Class9.2',
+    'Class9.3', 'Class10.1', 'Class10.2', 'Class10.3', 'Class11.1', 'Class11.2',
+    'Class11.3', 'Class11.4', 'Class11.5', 'Class11.6']
+
+    #hace las predicciones
+    pred = model.predict(x_test)
+
+    return [pred, ids]
