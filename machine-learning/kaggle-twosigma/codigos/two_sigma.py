4f3dfe33e01784e03408a7aafaa934fe510810ed
David <davidricardo888@hotmail.com>
2018-12-06 19:51:42 -0600

    Backup

--- machine-learning/kaggle-twosigma/codigos/two_sigma.py
+++ machine-learning/kaggle-twosigma/codigos/two_sigma.py
@@ -3,14 +3,16 @@
 import pandas as pd
 import numpy as np
 from sklearn import svm
+from copy import deepcopy
 
+#'returnsOpenNextMktres10' = r_{it}
 
-#Función para obtener un dataframe con los atributos
+#Función para obtener un los atributos y las clases
 #para un assetName en específico
 def obten_atributos(datos, assetName,atributos, universo_bool = True):
 	'''
 	ENTRADA
-	datos: Pandas dataframe con los datos sin filtrar
+	datos: Pandas dataframe con los datos sin filtrar, pero ya etiquetados
 
 	assetName: String con el assetName
 
@@ -20,17 +22,97 @@ def obten_atributos(datos, assetName,atributos, universo_bool = True):
 	(sólo para conjunto de entrenamiento)
 
 	SALIDA
-	dataframe con los datos únicamente para el assetName y los atributos.
+	atributos_filtrado: dataframe con los datos únicamente para el assetName y los atributos.
+
+	clases: Pandas series con las clases correspondientes a atributos_filtrado
 
 	'''
 
+	#Si es conjunto de entrenamiento
 	if universo_bool:
 		filtrado = datos[datos['assetName'] == assetName]
-		filtrado = filtrado[filtrado['universe'] == 1].loc[:,atributos]
-	else:
-		fitlrado = datos[(datos['assetName'] == assetName)].loc[:,atributos]
+		filtrado = filtrado[filtrado['universe'] == 1]
+		clases = filtrado['clase']
+		atributos_filtrado = filtrado.loc[:,atributos]
+		return atributos_filtrado, clases
 
-	return filtrado	
+	#Si es conjunto de prueba	
+	else:
+		filtrado = datos[(datos['assetName'] == assetName)]
+		atributos_filtrado = filtrado.loc[:,atributos]
+		return atributos_filtrado
+
+#Función para etiquetar el conjunto de entrenamiento
+def etiqueta(datos):
+	'''
+	ENTRADA
+	datos: Pandas dataframe con los datos sin etiquetar
+
+	SALIDA
+	El pandas dataframe 'datos' con la columna 'clase' añadida
+
+	'''
+
+	#copia los datos
+	copia = deepcopy(datos)
+
+	#índices en donde la variable 'returnsOpenNextMktres10' es positiva
+	indices = datos[datos['returnsOpenNextMktres10'] > 0].index
+
+	#agrega la columna clase
+	copia['clase'] = -1
+	copia.loc[indices,'clase'] = 1
+
+	return copia
+
+#Función para ajustar un modelo SVM	
+def ajusta_svm(atributos_asset, clases_asset):
+	'''
+	ENTRADA
+	atributos_asset: Pandas dataframe con los atributos para un assetName determinado
+
+	clases_asset: Pandas series con las clases para un assetName determinado
+
+	(Ver función obten_atributos con universo_bool = True)
+
+	SALIDA
+	modelo SVM ajustado
+	'''
+	#Define modelo e hiperparámetros
+	modelo = svm.SVC(probability = True, kernel = 'rbf', C = 1.4)
+
+	#ajusta modelo
+	modelo.fit(atributos_asset, clases_asset)
+
+	return modelo
+
+
+#Función para predecir las probabilidades de pertenecer a una clase
+#de acuerdo a un modelo (este modelo debe permitir el método predict_proba)	
+def predice_confianza(modelo, atributos_asset):
+	'''
+	ENTRADA
+	modelo: Modelo ajustado que permite el método predict_proba
+
+	atributos_asset: Pandas dataframe con los atributos para un assetName determinado
+	(idealmente, un dataframe con solamente un renglón)
+	(Ver función obten_atributos con universo_bool = False)
+
+	SALIDA
+	confianza con signo.
+	'''
+	#Obtiene las probabilidades
+	probas = modelo.predict_proba(atributos_asset)
+
+	#argumento de proba más grande
+	arg_proba_max = np.argmax(probas)
+
+	#Obtiene la confianza (con signo)
+	confianza = probas[0][arg_proba_max]*modelo.classes_[arg_proba_max]
+
+	return confianza
+
+		
 		
 #Atributos
 atributos = ('returnsClosePrevRaw1', 'returnsOpenPrevRaw1', 'returnsClosePrevMktres1', 
@@ -52,6 +134,9 @@ env = twosigmanews.make_env()
 #si utilizamos dropna(), se pierde menos del 3% de la información
 #así que supongo que no perdemos nada si lo hacemos :p
 market_train_df = market_train_df.dropna()
+
+#Conjunto de datos etiquetado
+etiquetado = etiqueta(market_train_df)
 
 #Obtiene los assetCodes sin repeticiones
 assetCodes_unicos = np.unique(market_train_df['assetCode'])
